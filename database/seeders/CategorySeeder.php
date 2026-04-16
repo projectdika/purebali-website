653538d389b767ad6e59a0c8f169b8727fd82191
satriadev <satriahgm@gmail.com>
2026-04-16 14:50:30 +0800

    feat: create login and register backend

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -12,6 +12,12 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = ["Tari Bali", "Benda Kesenian", "Aksara Bali", "Adat Istiadat"];
+
+        foreach($categories as $categoryName) {
+            Category::create([
+                "name" => $categoryName,
+            ]);
+        }
     }
 }
